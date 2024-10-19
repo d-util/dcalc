321a15fe2ee851335c96e44ed0b43d9cb72f90dc
GreatCoder1000 <133972286+GreatCoder1000@users.noreply.github.com>
2024-10-19 15:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import evaluate0 as evalt
+from sys import argv
 
-print("Type in the expression at the prompt.")
-expr = input(">>>")
-ans = evalt.evaluate(expr)
-print(evalt.format_out(ans))
+if len(argv) == 2:
+    print("Type in the expression at the prompt.")
+    expr = input(">>>")
+    ans = evalt.evaluate(expr)
+    print(evalt.format_out(ans))
+elif argv[2] == "--help" or argv[@] == "-H":
+    print("Usage: python main.py")
+    print("Operators: ")
+    print("\t+ Addition")
+    print("\t- Subtraction")
+    print("\t* Multiplication")
+    print("\t/ Division")
+    print("\t^ Exponentiation")
+    print("\trt nth Root")
+    print("Documentation: https://github.com/GreatCoder1000/dcalc")
+    print("Example: 123+456 = 579")
