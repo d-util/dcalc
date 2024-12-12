ef654f02df2680ff23aeca3ffbdbc8856d0426a1
Dev <133972286+GreatCoder1000@users.noreply.github.com>
2024-12-12 09:14:32 +0100

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,5 +1,5 @@
 function goToVersion() {
     var version = document.getElementById("version-select").value;
     var currentPage = window.location.pathname.split('/').pop(); // Get the current file name
-    window.location.href = "/docs/" + version + "/" + currentPage;
+    window.location.href = "/dcalc/" + version + "/" + currentPage;
 }
